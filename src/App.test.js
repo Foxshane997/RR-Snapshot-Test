ea81b23af1257da25318d920940e4f3185825407
“Foxshane997” <Foxshane997@gmail.com>
2024-06-17 23:55:33 -0700

    A snapshot runs and you can update it after adding to app.js

---
+++ src/App.test.js
@@ -0,0 +1,9 @@
+import App from './App'
+import renderer from 'react-test-renderer'
+
+// This first test is commented out since we have the other snapshot test that checks the GitHubCard component
+test('renders a snapshot', () => {
+  const tree = renderer.create(<App/>).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
